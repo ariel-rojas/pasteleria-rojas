d183d6cf1541f81245be7764287cd9c349be7e62
Ariel Rojas <arirojas50@gmail.com>
2022-07-12 16:51:00 -0300

    error renderizar productos

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,9 +4,9 @@ const Item = (props) => {
     return ( 
         <>
         <div className = "flex flex-col w-[250px] h-[500px]">
-            {/* <img className = "w-100" src={props.imgUrl} alt="Sunset in the mountains"></img> */}
+            <img className = "w-100" src={props.imgUrl} alt="Sunset in the mountains"></img>
             <div className="w-full">
-                <h3>{props}</h3>
+                <h3>{props.name}</h3>
                 <p>{props.description}</p>
                 <p>{props.price}</p>
                 <ItemCount stock = {10} initial = {0} onAdd ={() => console.log("se agregó al carrito")} />
