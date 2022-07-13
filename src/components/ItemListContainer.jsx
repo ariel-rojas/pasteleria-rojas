fad33bc3194e9fcf6cc4d0353dc294b4666ac1a2
arirojas <arirojas50@gmail.com>
2022-07-13 11:11:05 -0300

    no funca promesa itemlistcontainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,22 @@
 import ItemList from "./ItemList";
 import Item from "./Item"
+import { useEffect, setState } from "react";
 
-console.log(ItemList)
 
 const ItemListContainer = () => {
+    const [Items, setItems] = setState([])
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(resolve(ItemList),2000);
+        setTimeout(()=> reject("Error: no se han cargado los productos",2000))
+    })
+    useEffect(()=>{
+        promise.then((res) => setItems(ItemList))
+    }, [])
+    console.log(Items)
     return ( 
     <section className="w-full h-screen bg-zinc-200 drop-shadow-lg pt-[100px] ">
         <div className = "w-full h-screen flex flex-row flex-wrap justify-around items-center">
-            {ItemList.map((item) => {
+            {Items.map((item) => {
                 return <Item name={item.name} imgUrl={item.imgUrl} price={item.price} description={item.description}/>
             })}
         </div>   
