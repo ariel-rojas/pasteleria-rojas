d58671bf27eefe679e69a721a76355fb978b0d7f
Ariel Rojas <arirojas50@gmail.com>
2022-07-24 20:41:28 -0300

    detail funcional

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -3,23 +3,23 @@ import ItemCount from './ItemCount';
 
 // array de un elemento 
 const ItemDetail = ({item}) => {
-    // console.log('el objeto es:')
-    // console.log(item)
+    console.log('el objeto es:')
+    console.log(item)
     return ( 
         <>
         <div className='flex h-[500px] justify-center items-center bg-slate-50'>
-        <div className = "flex flex-col w-[400px] h-[400px] m-[20px] text-center bg-center bg-no-repeat bg-contain" style={{backgroundImage: `url(${item[0].img1URL})`}}>
-        </div>
-        <div className = "flex flex-col w-[400px] h-[400px] m-[20px] text-center">
-            {/* <img className = "w-100" src={item.imgURL} alt="album musical"></img> */}
-            <div className="w-full h-[300px] flex flex-col justify-center text-center">
-                <h3 className='min-h-[4rem]'>{item[0].name}</h3>
-                <p>Artista: {item[0].artists}</p>
-                <p>Precio: ${item[0].price}</p>
-                <p>Lanzamiento: {item[0].year}</p>
-                <ItemCount stock = {10} initial = {0} onAdd ={() => console.log("se agregó al carrito")} />
+            <div className = "flex flex-col w-[400px] h-[400px] m-[20px] text-center bg-center bg-no-repeat bg-contain" style={{backgroundImage: `url(${item.img1URL})`}}>
             </div>
-        </div>
+            <div className = "flex flex-col w-[400px] h-[400px] m-[20px] text-center">
+                {/* <img className = "w-100" src={item.imgURL} alt="album musical"></img> */}
+                <div className="w-full h-[300px] flex flex-col justify-center text-center">
+                    <h3 className='min-h-[4rem]'>{item.name}</h3>
+                    <p>Artista: {item.artists}</p>
+                    <p>Precio: ${item.price}</p>
+                    <p>Lanzamiento: {item.year}</p>
+                    <ItemCount stock = {10} initial = {0} onAdd ={() => console.log("se agregó al carrito")} />
+                </div>
+            </div>
         </div>
 
     </>        
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -5,19 +5,18 @@ import { useParams } from "react-router-dom";
 
 const ItemDetailContainer = () => {
     const { id } = useParams()
-    console.log(id)
+    console.log("el id es"+id)
     const [Item, setItems] = useState([])
 
     useEffect(() => {
         const loadData = new Promise((resolve, reject) => {
-            setTimeout(()=>resolve(albums),2000)
+            setTimeout(()=>resolve(albums.filter((album) => album.id == id).shift()),2000)
         })
         loadData.then((items) => {
-            setItems(items.filter((album) => 
-                album.id == id))
+            setItems(items)
         })
     },[id]) 
-    return ( 
+    return (
     <section className="w-full h-screen bg-zinc-200 drop-shadow-lg pt-[100px] ">
         <ItemDetail item = {Item}/>
     </section>
